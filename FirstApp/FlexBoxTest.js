ca35fab205e8ea8ff7b8f99d43e638825f00a04f
lorry0508 <1165354824@qq.com>
2020-07-17 14:25:57 +0800

    react-native 布局2

--- FirstApp/FlexBoxTest.js
+++ FirstApp/FlexBoxTest.js
@@ -5,18 +5,21 @@ import { Text, View } from 'react-native';
 export default class FlexBoxTest extends Component {
     render() {
         return (
-            <View style={ {flexDirection: 'row-reverse',backgroundColor:"darkgray",marginTop:20, flexWrap: 'wrap'}}>
-                <View style={ {width:100,height:40,backgroundColor:"darkcyan",margin:5}}>
+            <View style={ {flexDirection: 'column',justifyContent: 'center',alignItems: 'center', backgroundColor:"darkgray",marginTop:20, height: 300, borderWidth:1,}}>
+                <View style={ {alignSelf: 'flex-end',width:100,height:40,backgroundColor:"darkcyan",margin:5}}>
                     <Text style={ {fontSize:16}}>1</Text>
                 </View>
                 <View style={ {width:100,height:40,backgroundColor:"darkcyan",margin:5}}>
                     <Text style={ {fontSize:16}}>2</Text>
                 </View>
-                <View style={ {width:100,height:40,backgroundColor:"darkcyan",margin:5}}>
+                <View style={ {alignSelf: 'flex-start',flex: 1,width:100,height:40,backgroundColor:"darkcyan",margin:5}}>
                     <Text style={ {fontSize:16}}>3</Text>
                 </View>
                 <View style={ {width:100,height:40,backgroundColor:"darkcyan",margin:5}}>
                     <Text style={ {fontSize:16}}>4</Text>
+                </View>
+                <View style={ {alignSelf: 'flex-end',width:100,height:40,backgroundColor:"darkcyan",margin:5}}>
+                    <Text style={ {fontSize:16}}>5</Text>
                 </View>
             </View>
         );
