bdeb870578346300bb8a258649980856bae40f81
lorry0508 <1165354824@qq.com>
2020-07-21 18:14:58 +0800

    创建喜马拉雅练习项目

---
+++ TS_react-native/ximalaya/src/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { 
+    View, 
+    Text 
+} from 'react-native';
+
+class App extends React.Component {
+    render() {
+        return (
+            <View>
+               <Text>app</Text>
+            </View>
+        );
+    }
+}
+
+export default App;
