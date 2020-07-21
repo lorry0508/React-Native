6814f9cc5cc5f357835580f88d8df35cc9513420
lorry0508 <1165354824@qq.com>
2020-07-21 14:55:41 +0800

    实现选择服务品类功能

--- TS_react-native/ReactNativeApp/src/pages/Home/subpages/Body/index.tsx
+++ TS_react-native/ReactNativeApp/src/pages/Home/subpages/Body/index.tsx
@@ -6,6 +6,7 @@ import {
 } from 'react-native';
 
 import SelectCareer from './SelectCareer';
+import SelectCategory from './SelectCategory';
 
 import {
     careerList,
@@ -16,6 +17,10 @@ interface IState {
     selectedCareerId: number;
     isSelectedCareer: boolean;
     canSelectLength: number;
+    selectedCategoryIds: string[];
+    selectedTypeIdLists: number[][];
+    selectedServiceAreaIds: number[][];
+    selectedServicePromiseIds: number[];
 }
 
 class Body extends React.PureComponent<{}, IState> {
@@ -24,7 +29,11 @@ class Body extends React.PureComponent<{}, IState> {
         this.state = {
             selectedCareerId: 0,
             isSelectedCareer: false,
-            canSelectLength: 3, // 可以选择的服务品类种数
+            canSelectLength: 3,   // 可以选择的服务品类个数
+            selectedCategoryIds: [],
+            selectedTypeIdLists: [],
+            selectedServiceAreaIds: [],
+            selectedServicePromiseIds: [],
         }
     }
     onChangeCareer = (career: ICareer) => {
@@ -35,8 +44,34 @@ class Body extends React.PureComponent<{}, IState> {
             canSelectLength: career.id === 0 ? 5 : 3,
         })
     }
+    // 保存服务类目和服务类型
+    saveCategory = (selectedCategoryId: string, selectedTypeIds: number[], currentIndex: number) => {
+        const { selectedCategoryIds, selectedTypeIdLists } = this.state;
+        const newSelectedCategoryIds = [ ...selectedCategoryIds ];
+        const newSelectedTypeIdLists = [ ...selectedTypeIdLists ];
+        newSelectedCategoryIds[currentIndex] = selectedCategoryId;
+        newSelectedTypeIdLists[currentIndex] = selectedTypeIds;
+        this.setState({
+        selectedCategoryIds: newSelectedCategoryIds,
+        selectedTypeIdLists: newSelectedTypeIdLists,
+        });
+    }
+    clean = () => {
+        this.setState({
+          selectedCategoryIds: [],
+          selectedTypeIdLists: [],
+        })
+    }
     render() {
-        const { selectedCareerId, isSelectedCareer  } = this.state;
+        const {
+            selectedCareerId, 
+            isSelectedCareer,
+            canSelectLength,
+            selectedCategoryIds,
+            selectedTypeIdLists,
+            selectedServiceAreaIds,
+            selectedServicePromiseIds,
+        } = this.state;
         return (
             <View>
                 <SelectCareer
@@ -44,6 +79,13 @@ class Body extends React.PureComponent<{}, IState> {
                     selectedCareerId={selectedCareerId}
                     isSelectedCareer={isSelectedCareer}
                     onChangeCareer={this.onChangeCareer}
+                />
+                <SelectCategory 
+                    canSelectLength={canSelectLength}
+                    saveCategory={this.saveCategory}
+                    selectedCategoryIds={selectedCategoryIds}
+                    selectedTypeIdLists={selectedTypeIdLists}
+                    clean={this.clean}
                 />
             </View>
         );
