e3d391685f6d5c0054b4fb8827941caa5320f4ca
lorry0508 <1165354824@qq.com>
2020-07-22 11:51:17 +0800

    项目引入原生的一些库

--- TS_react-native/ximalaya/babel.config.js
+++ TS_react-native/ximalaya/babel.config.js
@@ -1,3 +1,20 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+	presets: ['module:metro-react-native-babel-preset'],
+	plugin: [
+		[
+			'module-resolver',
+			{
+				root: ['./src'],
+				alias: {
+					'@/assets': './src/assets',
+					'@/components': './src/components',
+					'@/config': './src/config',
+					'@/models': './src/models',
+					'@/navigator': './src/navigator',
+					'@/pages': './src/pages',
+					'@/utils': './src/utils'
+				}
+			}
+		]
+	]
 };
