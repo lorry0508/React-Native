f01a2da6dc9a02bb7eaa9ccaa9e0f4f55dd379eb
lorry0508 <1165354824@qq.com>
2020-07-31 10:13:22 +0800

    类别模块的model

---
+++ TS_react-native/ximalaya/ximalaya/src/models/category.ts
@@ -0,0 +1,96 @@
+import { Model, Effect, SubscriptionsMapObject } from "dva-core-ts";
+import { Reducer } from "redux";
+import storage from "@/config/storage";
+import axios from "axios";
+
+const CATEGORY_URL = '/category';
+
+interface ICategoty {
+    id: string;
+    name: string;
+    classify?: string;
+}
+
+interface CategoryModelState {
+    myCategorys: ICategoty[];
+    categorys: ICategoty[];
+}
+
+interface CategoryModel extends Model {
+    namespace: 'category';
+    state: CategoryModelState;
+    effects: {
+        loadData: Effect,
+    };
+    reducers: {
+        setState: Reducer<CategoryModelState>
+    };
+    subscriptions: SubscriptionsMapObject;
+}
+
+const initialState = {
+    myCategorys: [
+        {
+            id: 'home',
+            name: '推荐'
+        },
+        {
+            id: 'vip',
+            name: 'Vip'
+        }
+    ],
+    categorys: []
+};
+
+const categoryModel: CategoryModel = {
+    namespace: 'category',
+    state: initialState,
+    effects: {
+        *loadData(_, { call, put }) {
+            // 从storage中获取数据
+            const myCategorys = yield call(load, { key: 'myCategorys' });
+            const categorys = yield call(load, { key: 'categorys' });
+            // 发起action，将数据保存到state
+            if (myCategorys) {
+                yield put({
+                    type: 'setState',
+                    payload: {
+                        myCategorys,
+                        categorys
+                    }
+                })
+            } else {
+                yield put({
+                    type: 'setState',
+                    payload: {
+                        categorys
+                    }
+                })
+            }
+        }
+    },
+    reducers: {
+        setState(state, { payload }) {
+            return {
+                ...state,
+                ...payload
+            }
+        }
+    },
+    subscriptions: {
+        setup({ dispatch }) {
+            dispatch({ type: 'loadData' });
+        },
+        asyncStorage() {
+            storage.sync.categorys = async () => {
+                const { data } = await axios.get(CATEGORY_URL);
+                return data.data;
+            }
+            storage.sync.myCategorys = async () => {
+                return null;
+            }
+        }
+    }
+};
+
+export default categoryModel;
--- TS_react-native/ximalaya/ximalaya/src/models/index.ts
+++ TS_react-native/ximalaya/ximalaya/src/models/index.ts
@@ -1,10 +1,12 @@
-import home from './home';
 import { DvaLoadingState } from 'dva-loading-ts';
+import home from './home';
+import category from './category';
 
-const models = [home];
+const models = [home, category];
 
 export type RootState = {
     home: typeof home.state;
+    category: typeof category.state;
     loading: DvaLoadingState
 };
 
